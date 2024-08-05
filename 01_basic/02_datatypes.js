8a117bebc4778eded06a8a7105f5e28b64465f25
Akash kumar Tripathy <163890789+Aktcse@users.noreply.github.com>
2024-08-05 17:56:11 +0000

    command config

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,27 @@
+"Use strict"; // treat all js code as newer version
+
+//alert("AKT") // uwe are using nodejs, not browser
+
+//console.log(5+5) console.log("Akash") //error
+console.log(5+5) 
+console.log("Akash")
+
+let name="Akash"
+let age=34
+let isLogin=false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof(age))
+console.log(typeof(null)) //o/p=> object
+console.log(typeof(undefined)) //o/p=>undefined
+//////
