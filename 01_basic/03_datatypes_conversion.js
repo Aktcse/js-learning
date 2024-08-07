a96768c65e77b26f16d7a6079bbb53dc77393726
Akash kumar Tripathy <163890789+Aktcse@users.noreply.github.com>
2024-08-07 17:42:33 +0000

    command config

---
+++ 01_basic/03_datatypes_conversion.js
@@ -0,0 +1,24 @@
+let score="33"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueinNumber = Number(score)
+
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+
+let num=1;
+let bool =Boolean(num)
+console.log(bool)
+
+
+//1=>true; 0=>false
+//""=>false
+//"Akash"=>true
